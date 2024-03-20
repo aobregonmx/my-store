4a42084d0a616389f4fb57650bea85c72a20bdfa
Antonio Obregon <antonio.obregon@utict.unam.mx>
2024-03-20 13:07:48 -0600

    Cambio de puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routerApi = require ('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = 8008;
 
 app.use(express.json());
 
